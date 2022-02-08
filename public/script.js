47e623225da53f9075aa73061d96fff3216e093e
yisusworldwide <jesusjimenez.email@gmail.com>
2022-02-08 18:17:10 +0100

    DOMContentLoaded
    You do that so you know all the parsed elements are available in the DOM.
    postData, removeData and updateData aren't defined.

--- public/script.js
+++ public/script.js
@@ -1,85 +1,88 @@
-displayPerceptions();
+window.addEventListener("DOMContentLoaded", function() {
+    
+        displayPerceptions();
 
-function _(id) {
-    return document.getElementById(id);
-    this.init();
-}
-
-function postData(variable){
-    var s = document.getElementById(variable);
-    addPerception(s.value);
-    setTimeout(displayPerceptions, 1000);
-    s.value = ''
-    this.init();
-}
-
-function removeData(id){
-    removePerception(id);
-    setTimeout(displayPerceptions, 1000);
-    this.init();
-}
-
-function updateData(variable){
-    var s = document.getElementById(variable);
-    updatePerception(s.value);
-    setTimeout(displayPerceptions, 1000);
-    s.value = ''
-}
-
-Date.prototype.addHours = function(h) {
-    this.setTime(this.getTime() + (h*60*60*1000));
-    return this;
-}
-
-function displayPerceptions() {
-    axios.get('http://localhost:3000/perceptions').then((res) => {
-        var rows = res.data
-        console.log(rows)
-        _('rs').innerHTML = '';
-        for (var i = 0; i < rows.length; i++) {
-            let txt = rows[i].text;
-            let id = rows[i].id;
-            const d = new Date(rows[i].created_at);
-            d.addHours(1)
-            _('rs').innerHTML += `
-            <article class="message">
-            <div class="message-header">
-                <p style='color:white;'>Percepción #${id}</p>
-                <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
-                <button class="update" aria-label="update" onclick="updateData(${id})"></button>
-            </div>
-            <div class="message-body">
-            <div>${txt}</div>
-            <div><small>${d.toLocaleString()}</small></div>
-            </div>
-            </article>
-            `;
+        function _(id) {
+            return document.getElementById(id);
         }
-    })
-}
+        
+        function postData(variable){
+            var s = document.getElementById(variable);
+            addPerception(s.value);
+            setTimeout(displayPerceptions, 1000);
+            s.value = ''
+        }
+        
+        function removeData(id){
+            removePerception(id);
+            setTimeout(displayPerceptions, 1000);
+        }
+        
+        function updateData(variable){
+            var s = document.getElementById(variable);
+            updatePerception(s.value);
+            setTimeout(displayPerceptions, 1000);
+            s.value = ''
+        }
+        
+        Date.prototype.addHours = function(h) {
+            this.setTime(this.getTime() + (h*60*60*1000));
+            return this;
+        }
+        
+        function displayPerceptions() {
+            axios.get('http://localhost:3000/perceptions').then((res) => {
+                var rows = res.data
+                console.log(rows)
+                _('rs').innerHTML = '';
+                for (var i = 0; i < rows.length; i++) {
+                    let txt = rows[i].text;
+                    let id = rows[i].id;
+                    const d = new Date(rows[i].created_at);
+                    d.addHours(1)
+                    _('rs').innerHTML += `
+                    <article class="message">
+                    <div class="message-header">
+                        <p style='color:white;'>Percepción #${id}</p>
+                        <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
+                        <button class="update" aria-label="update" onclick="updateData(${id})"></button>
+                    </div>
+                    <div class="message-body">
+                    <div>${txt}</div>
+                    <div><small>${d.toLocaleString()}</small></div>
+                    </div>
+                    </article>
+                    `;
+                }
+            })
+        }
+        
+        function addPerception(text) {
+            let payload = { text: text };
+            console.log('script ' + payload.text)
+            let res = axios.post('http://localhost:3000/perceptions', payload);
+            let data = res.data;
+            console.log(data);
+        }
+        
+        function removePerception(id) {
+            let res = axios.delete('http://localhost:3000/perceptions/' + id);
+            let data = res.data;
+            console.log(data);
+        }
+        
+        
+        function updatePerception(text) {
+            let payload = { text: text };
+            console.log('script ' + payload.text)
+            let res = axios.put('http://localhost:3000/perceptions/', payload);
+            let data = res.data;
+            console.log(data);
+        }
+        
+    }, false);
 
-function addPerception(text) {
-    let payload = { text: text };
-    console.log('script ' + payload.text)
-    let res = axios.post('http://localhost:3000/perceptions', payload);
-    let data = res.data;
-    console.log(data);
-}
 
-function removePerception(id) {
-    let res = axios.delete('http://localhost:3000/perceptions/' + id);
-    let data = res.data;
-    console.log(data);
-}
-
-
-function updatePerception(text) {
-    let payload = { text: text };
-    console.log('script ' + payload.text)
-    let res = axios.put('http://localhost:3000/perceptions/', payload);
-    let data = res.data;
-    console.log(data);
-}
 
 // function initPage() {}
 // window.addEventListener("load", initPage, false);
