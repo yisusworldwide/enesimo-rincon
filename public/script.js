a95cac03e1358951fdc3e8080155c8a107a3ff0d
yisusworldwide <jesusjimenez.email@gmail.com>
2022-02-13 23:51:33 +0100

    Restructuring functions
    Function is declared but its value is never read.

--- public/script.js
+++ public/script.js
@@ -1,86 +1,92 @@
-// document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
+
+    function addPerception(text) {
+        let payload = { text: text };
+        console.log('script ' + payload.text)
+        let res = axios.post('http://localhost:3000/perceptions', payload);
+        let data = res.data;
+        console.log(data);
+    }
     
-        displayPerceptions();
+    function removePerception(id) {
+        let res = axios.delete('http://localhost:3000/perceptions/' + id);
+        let data = res.data;
+        console.log(data);
+    }
+    
+    function updatePerception(text) {
+        let payload = { text: text };
+        console.log('script ' + payload.text)
+        let res = axios.put('http://localhost:3000/perceptions/', payload);
+        let data = res.data;
+        console.log(data);
+    }  
 
-        function _(id) {
-            return document.getElementById(id);
-        }
+    //LINKED-TREE FUNCTIONS TO ABOVE ONES
 
-        function displayPerceptions() {
-            axios.get('http://localhost:3000/perceptions').then((res) => {
-                var rows = res.data
-                console.log(rows)
-                _('rs').innerHTML = '';
-                for (var i = 0; i < rows.length; i++) {
-                    let txt = rows[i].text;
-                    let id = rows[i].id;
-                    const d = new Date(rows[i].created_at);
-                    d.addHours(1)
-                    _('rs').innerHTML += `
-                    <article class="message">
-                    <div class="message-header">
-                        <p style='color:white;'>Percepción #${id}</p>
-                        <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
-                        <button class="update" aria-label="update" onclick="updateData(${id})"></button>
-                    </div>
-                    <div class="message-body">
-                    <div>${txt}</div>
-                    <div><small>${d.toLocaleString()}</small></div>
-                    </div>
-                    </article>
-                    `;
-                }
-            })
-        }
 
-        Date.prototype.addHours = function(h) {
-            this.setTime(this.getTime() + (h*60*60*1000));
-            return this;
-        }
-        
-        function addPerception(text) {
-            let payload = { text: text };
-            console.log('script ' + payload.text)
-            let res = axios.post('http://localhost:3000/perceptions', payload);
-            let data = res.data;
-            console.log(data);
-        }
-        
-        function removePerception(id) {
-            let res = axios.delete('http://localhost:3000/perceptions/' + id);
-            let data = res.data;
-            console.log(data);
-        }
-        
-        function updatePerception(text) {
-            let payload = { text: text };
-            console.log('script ' + payload.text)
-            let res = axios.put('http://localhost:3000/perceptions/', payload);
-            let data = res.data;
-            console.log(data);
-        }  
+    function postData(variable){
+        var s = document.getElementById(variable);
+        addPerception(s.value);
+        setTimeout(displayPerceptions, 1000);
+        s.value = ''
+    }
+    
+    function removeData(id){
+        removePerception(id);
+        setTimeout(displayPerceptions, 1000);
+    }
+    
+    function updateData(variable){
+        var s = document.getElementById(variable);
+        updatePerception(s.value);
+        setTimeout(displayPerceptions, 1000);
+        s.value = ''
+    }
+              
+}); 
 
-        //LINKED-TREE FUNCTIONS TO ABOVE ONES
+window.addEventListener('load', function(){
 
-        function postData(variable){
-            var s = document.getElementById(variable);
-            addPerception(s.value);
-            setTimeout(displayPerceptions, 1000);
-            s.value = ''
-        }
-        
-        function removeData(id){
-            removePerception(id);
-            setTimeout(displayPerceptions, 1000);
-        }
-        
-        function updateData(variable){
-            var s = document.getElementById(variable);
-            updatePerception(s.value);
-            setTimeout(displayPerceptions, 1000);
-            s.value = ''
-        }
-        
-    // }, false); 
+    displayPerceptions();
 
+    function _(id) {
+        return document.getElementById(id);
+    }
+
+    function displayPerceptions() {
+        axios.get('http://localhost:3000/perceptions').then((res) => {
+            var rows = res.data
+            console.log(rows)
+            _('rs').innerHTML = '';
+            for (var i = 0; i < rows.length; i++) {
+                let txt = rows[i].text;
+                let id = rows[i].id;
+                const d = new Date(rows[i].created_at);
+                d.addHours(1)
+                _('rs').innerHTML += `
+                <article class="message">
+                <div class="message-header">
+                    <p style='color:white;'>Percepción #${id}</p>
+                    <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
+                    <button class="update" aria-label="update" onclick="updateData(${id})"></button>
+                </div>
+                <div class="message-body">
+                <div>${txt}</div>
+                <div><small>${d.toLocaleString()}</small></div>
+                </div>
+                </article>
+                `;
+            }
+        })
+    }
+
+    Date.prototype.addHours = function(h) {
+        this.setTime(this.getTime() + (h*60*60*1000));
+        return this;
+    }
+
+});
+
+// }, false);
 // function initPage() {}
