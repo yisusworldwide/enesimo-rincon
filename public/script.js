417f1d0fcf2660bbf3fda00795c7ec43e24a0025
yisusworldwide <jesusjimenez.email@gmail.com>
2022-03-10 19:24:31 +0100

    removed addEventListeners
    Removed 'DOMContentLoaded' and window.addEventListener('load', function() being not appliable any more. Also realigned the body code in script.js.

--- public/script.js
+++ public/script.js
@@ -1,101 +1,89 @@
-// document.addEventListener('DOMContentLoaded', () => {
+displayPerceptions();
 
-    displayPerceptions();
+function _(id) {
+    return document.getElementById(id);
+}
 
-    function _(id) {
-        return document.getElementById(id);
-    }
+function displayPerceptions() {
+    axios.get('http://localhost:3000/perceptions').then((res) => {
+        var rows = res.data
+        console.log(rows)
+        _('rs').innerHTML = '';
+        for (var i = 0; i < rows.length; i++) {
+            let txt = rows[i].text;
+            let id = rows[i].id;
+            const d = new Date(rows[i].created_at);
+            d.addHours(1)
+            _('rs').innerHTML += `
+            <article class="message">
+            <div class="message-header">
+                <p style='color:white;'>Percepción #${id}</p>
+                <button class="update" aria-label="update" onclick="updateData(${id})"></button>
+                <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
+            </div>
+            <div class="message-body">
+            <div id= "realText">${txt}</div>
+            <div><small>${d.toLocaleString()}</small></div>
+            </div>
+            </article>
+            `;
+        }
+    })
+}
 
-    function displayPerceptions() {
-        axios.get('http://localhost:3000/perceptions').then((res) => {
-            var rows = res.data
-            console.log(rows)
-            _('rs').innerHTML = '';
-            for (var i = 0; i < rows.length; i++) {
-                let txt = rows[i].text;
-                let id = rows[i].id;
-                const d = new Date(rows[i].created_at);
-                d.addHours(1)
-                _('rs').innerHTML += `
-                <article class="message">
-                <div class="message-header">
-                    <p style='color:white;'>Percepción #${id}</p>
-                    <button class="update" aria-label="update" onclick="updateData(${id})"></button>
-                    <button class="delete" aria-label="delete" onclick="removeData(${id})"></button>
-                </div>
-                <div class="message-body">
-                <div id= "realText">${txt}</div>
-                <div><small>${d.toLocaleString()}</small></div>
-                </div>
-                </article>
-                `;
-            }
-        })
-    }
+Date.prototype.addHours = function(h) {
+    this.setTime(this.getTime() + (h*60*60*1000));
+    return this;
+}
 
-    Date.prototype.addHours = function(h) {
-        this.setTime(this.getTime() + (h*60*60*1000));
-        return this;
-    }
-
-// }); 
-
-// window.addEventListener('load', function(){
-
-    function addPerception(text) {
-        let payload = { text: text };
-        console.log('script ' + payload.text)
-        let res = axios.post('http://localhost:3000/perceptions', payload);
-        let data = res.data;
-        console.log(data);
-    }
+function addPerception(text) {
+    let payload = { text: text };
+    console.log('script ' + payload.text)
+    let res = axios.post('http://localhost:3000/perceptions', payload);
+    let data = res.data;
+    console.log(data);
+}
     
-    function removePerception(id) {
-        let res = axios.delete('http://localhost:3000/perceptions/' + id);
-        let data = res.data;
-        console.log(data);
-    }
+function removePerception(id) {
+    let res = axios.delete('http://localhost:3000/perceptions/' + id);
+    let data = res.data;
+    console.log(data);
+}
     
-    function updatePerception(text) {
-        let payload = { text: text };
-        console.log('script ' + payload.text)
-        let res = axios.put('http://localhost:3000/perceptions/' + payload);
-        let data = res.data;
-        console.log(data);
-    }  
+function updatePerception(text) {
+    let payload = { text: text };
+    console.log('script ' + payload.text)
+    let res = axios.put('http://localhost:3000/perceptions/' + payload);
+    let data = res.data;
+    console.log(data);
+}  
 
-    function postData(id){
-        let s = document.getElementById(id);
-        addPerception(s.value);
-        setTimeout(displayPerceptions, 1000);
-        s.value = ''
-    }
+function postData(id){
+    let s = document.getElementById(id);
+    addPerception(s.value);
+    setTimeout(displayPerceptions, 1000);
+    s.value = ''
+}
     
-    function removeData(id){
-        removePerception(id);
-        setTimeout(displayPerceptions, 1000);
-    }
+function removeData(id){
+    removePerception(id);
+    setTimeout(displayPerceptions, 1000);
+}
 
-    function updateData(txt){
-        let s = document.querySelector("#realText");
-        // let s = document.getElementById(id).textContent;
-        // if (document.getElementById(id) != null) {
-        // str = document.getElementById(id).value;
-        // }
-        updatePerception(s.value);
-        // updatePerception(s.innerHTML);
-        setTimeout(displayPerceptions, 1000);
-        s.value = ''
-        // s.innerHTML = ''
-    }
-
-    // function updateData(id){
-    //     updatePerception(id);
-    //     setTimeout(displayPerceptions, 1000);
+function updateData(){
+    let s = document.querySelector("#realText");
+    // let s = document.getElementById(id).textContent;
+    // if (document.getElementById(id) != null) {
+    // str = document.getElementById(id).value;
     // }
+    updatePerception(s.value);
+    // updatePerception(s.innerHTML);
+    setTimeout(displayPerceptions, 1000);
+    s.value = ''
+    // s.innerHTML = ''
+}
 
-// });
-
-
-// }, false);
-// function initPage() {}
+// function updateData(id){
+//     updatePerception(id);
+//     setTimeout(displayPerceptions, 1000);
+// }
