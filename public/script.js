a1311fa5ccf9f33c590fb621489b980c8b0b328e
yisusworldwide <jesusjimenez.email@gmail.com>
2023-09-08 00:56:08 +0200

    Inserted the event handlers for the JavaScript part

--- public/script.js
+++ public/script.js
@@ -36,7 +36,18 @@ function displayPerceptions() {
     })
 }
 
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
 
+    function switchTheme(e) {
+        if (e.target.checked) {
+            document.documentElement.setAttribute('data-theme', 'dark');
+        }
+        else {
+            document.documentElement.setAttribute('data-theme', 'light');
+        }    
+    }
+    
+    toggleSwitch.addEventListener('change', switchTheme, false);
 
 /* <form id="for" action="/perceptions/:id" method="post">
 <div class="message-body">
